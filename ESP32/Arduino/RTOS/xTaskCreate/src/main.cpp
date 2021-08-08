6cc1bc319624b8c950a035c6e0434be4bae79055
Petrus Candido <petrusz1@gmail.com>
2021-08-08 09:25:09 -0300

    adicionado exemplo básico do uso de tarefas no FreeRTOS p/ ESP32 no Arduino

---
+++ ESP32/Arduino/RTOS/xTaskCreate/src/main.cpp
@@ -0,0 +1,65 @@
+/**
+ * exemplo basico usando as apis do freertos
+ * 
+ */
+
+#include <Arduino.h>
+
+#define OUTPUT_PORT Serial
+#define LED 2
+#define DELAY 1000
+
+void taskOne(void *parameter)
+{
+
+  for (int i = 0; i < 10; i++)
+  {
+
+    OUTPUT_PORT.println("Hello from task 1");
+    delay(1000);
+  }
+
+  OUTPUT_PORT.println("Ending task 1");
+  vTaskDelete(NULL);
+}
+
+void taskTwo(void *parameter)
+{
+
+  for (int i = 0; i < 10; i++)
+  {
+
+    OUTPUT_PORT.println("Hello from task 2");
+    delay(1000);
+  }
+  OUTPUT_PORT.println("Ending task 2");
+  vTaskDelete(NULL);
+}
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+  OUTPUT_PORT.begin(115200);
+  OUTPUT_PORT.println("\n\nSistema Iniciado!\n");
+
+  xTaskCreate(
+      taskOne,   /* Task function. */
+      "TaskOne", /* String with name of task. */
+      10000,     /* Stack size in bytes. */
+      NULL,      /* Parameter passed as input of the task */
+      1,         /* Priority of the task. */
+      NULL);     /* Task handle. */
+
+  xTaskCreate(
+      taskTwo,   /* Task function. */
+      "TaskTwo", /* String with name of task. */
+      10000,     /* Stack size in bytes. */
+      NULL,      /* Parameter passed as input of the task */
+      1,         /* Priority of the task. */
+      NULL);     /* Task handle. */
+}
+
+void loop()
+{
+  delay(1000);
+}
