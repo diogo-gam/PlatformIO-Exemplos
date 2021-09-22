506f1ead8b7ba16a073bfe3ffe663f700f50655f
Petrus Candido de Lorena <petrusz1@gmail.com>
2021-09-22 18:09:34 -0300

    adicionados exemplos de pwm com hardwareTimer no stm32

--- STM32/Arduino/Soft_PWM/src/main.cpp
+++ STM32/Arduino/Soft_PWM/src/main.cpp
@@ -10,16 +10,22 @@
 #define CH1 PA4
 #define CH2 PA5
 #define CH3 PA6
+#define N_CHANNELS 3
 
-int chState = LOW;
+int channels[N_CHANNELS] = {CH1, CH2, CH3};
+// int chState[N_CHANNELS] = 0;
+int chState = 0;
 
 unsigned long previousMicros = 0;
 const long interval = 1;
-int channel = CH1;
 
 void setup()
 {
-  pinMode(channel, OUTPUT);
+  for (int i = 0; i < N_CHANNELS; i++)
+  {
+    pinMode(channels[i], OUTPUT);
+  }
+  
   OUTPUT_PORT.begin(115200);
   OUTPUT_PORT.println(F("\r\n\nSistema Iniciado:"));
 }
@@ -30,6 +36,9 @@ void loop()
   if(currentMicros - previousMicros >= interval) {
     previousMicros = currentMicros;
     chState = !chState;
-    digitalWrite(channel, chState);
+    for (int i = 0; i < N_CHANNELS; i++)
+    {
+      digitalWrite(channels[i], chState);
+    }
   }
 }
